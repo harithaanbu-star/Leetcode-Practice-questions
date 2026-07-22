8cb8499c613acccdaec4153768fb3581fc188df6
Haritha <harithaanbu34@gmail.com>
2026-07-22 13:54:49 +0530

    Time: 0 ms (100%), Space: 44.4 MB (35.79%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] runningSum(int[] nums) {
      int [] ans = new int [nums.length];
+     int sum=0;
      for(int i=0;i<nums.length;i++){
-        if(i ==0){
-            ans[i]=nums[i];
-        }else 
-        ans[i]=ans[i-1]+nums[i];
+        sum+=nums[i];
+        ans[i]=sum;
      }   return ans;
     }
 }
