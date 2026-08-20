77692e3853344a0b4c8abd7d59a56b76bb7d6e3e
Haritha <harithaanbu34@gmail.com>
2026-08-20 12:20:11 +0530

    Medium .java Hash Table

---
+++ LeetCode/Medium/0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        
+        HashMap<Node,Node> map = new HashMap<>();
+        Node cur = head;
+        while(cur !=null){
+            map.put(cur, new Node(cur.val));
+            cur=cur.next;
+        }
+        cur=head;
+        while(cur !=null){
+            Node copy = map.get(cur);
+            copy.next=map.get(cur.next);
+            copy.random = map.get(cur.random);
+            cur=cur.next;
+        }
+        return map.get(head);
+    }
+}
