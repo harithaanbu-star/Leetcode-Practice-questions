bcb2bb67a860bd936aa21f2c5f4e38d0e087cfa2
Haritha <harithaanbu34@gmail.com>
2026-08-27 09:35:40 +0530

    Easy .java Tree

--- LeetCode/Easy/0112-path-sum/0112-path-sum.java
+++ LeetCode/Easy/0112-path-sum/0112-path-sum.java
@@ -14,30 +14,15 @@
  * }
  */
 class Solution {
-    static 
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        Queue<TreeNode> q = new LinkedList<>();
         if(root==null) return false;
-        Queue<Integer> sum = new LinkedList<>();
-        q.offer(root);
-        
-        sum.offer(root.val);
-        while(!q.isEmpty()){
-            TreeNode cur=q.poll();
-            int cursum =sum.poll();
-            if(cur.left == null && cur.right==null){
-                if(cursum==targetSum) return true;
+        if(root.left==null && root.right==null){
+            if(targetSum-root.val==0){
+                return true;
+            }else{
+                return false;
             }
-            if(cur.left!=null){
-                q.offer(cur.left);
-                sum.offer(cursum+cur.left.val);
-            }
-            if(cur.right!=null){
-                q.offer(cur.right);
-                sum.offer(cursum+cur.right.val);
-            }
-
         }
-        return false;
+        return hasPathSum(root.left ,targetSum-root.val) ||hasPathSum(root.right ,targetSum-root.val);
     }
 }
