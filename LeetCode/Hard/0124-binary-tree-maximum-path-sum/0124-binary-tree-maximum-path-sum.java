8f0b66a8968f3648feb7d1d3dc68c26b05cd0b4d
Haritha <harithaanbu34@gmail.com>
2026-08-27 11:52:04 +0530

    Hard .java Dynamic Programming

---
+++ LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int maxheight=Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        return Math.max(maxpath(root),maxheight);
+    }
+
+    public int maxpath(TreeNode root){
+        if(root==null) return 0;
+        int left = Math.max(0,maxpath(root.left));
+        int right= Math.max(0,maxpath(root.right));
+        int center=root.val +left+right;
+        this.maxheight=Math.max(this.maxheight,center);
+        return root.val+Math.max(left,right);
+    }
+}
