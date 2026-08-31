b38b4b17fc0e60fd8febe1f3456d059712eab5a4
Haritha <harithaanbu34@gmail.com>
2026-08-31 08:19:17 +0530

    Easy .java Tree

---
+++ LeetCode/Easy/0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static TreeNode search(TreeNode root,int val){
+        if(root==null) return null;
+        if(root.val==val) return root;
+        if(val>root.val){
+            return search(root.right,val);
+        }else{
+            return search(root.left,val);
+        }
+    }
+    public TreeNode searchBST(TreeNode root, int val) {
+        return search(root,val);
+    }
+}
