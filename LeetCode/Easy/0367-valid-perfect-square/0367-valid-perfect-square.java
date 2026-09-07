40abef72a2fba0657f355cfe0aaea5269a081ea5
Haritha <harithaanbu34@gmail.com>
2026-09-07 19:10:46 +0530

    Easy .java Math

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long low =1;
+        long high =num;
+        while(low<=high){
+            long mid =low+(high-low)/2;
+            if(mid*mid == num && num%mid==0){
+                return true;
+            }
+            if(mid*mid<num){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
+}
