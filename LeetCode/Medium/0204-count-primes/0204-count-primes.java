106eb2a20a90de2987a7ec026777df8cf504b477
Haritha <harithaanbu34@gmail.com>
2026-09-08 10:15:57 +0530

    Medium .java Array

---
+++ LeetCode/Medium/0204-count-primes/0204-count-primes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countPrimes(int n) {
+        boolean[] prime = new boolean[n];
+        Arrays.fill(prime ,true);
+        if(n>1) prime[1]=false;
+        if(n>0) prime[0]=false;
+        for(int i=2;i*i<n;i++){
+            if(prime[i]){
+                for(int j=i*i;j<n;j+=i){
+                    prime[j]=false;
+                }
+            }
+        }
+        int cnt=0;
+        for(int i=2;i<n;i++){
+            if(prime[i]) cnt++;
+        }
+        return cnt;
+    }
+}
