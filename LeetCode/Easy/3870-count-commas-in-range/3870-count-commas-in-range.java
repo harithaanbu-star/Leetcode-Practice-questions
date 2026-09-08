8e0f65745cf08a2b2cc49030a24ebb9648b4e83b
Haritha <harithaanbu34@gmail.com>
2026-09-08 21:36:59 +0530

    Easy .java Math

--- LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
+++ LeetCode/Easy/3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,8 +1,6 @@
 class Solution {
     public int countCommas(int n) {
         if(n<1000) return 0;
-        if(n>=1000 && n<100000){
-            return n-999;
-        }return n-999;
+        return n-999;
     }
 }
