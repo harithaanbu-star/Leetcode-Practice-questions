b94fd3fb2f5b8598297145a7a31070d718536486
Haritha <harithaanbu34@gmail.com>
2026-09-11 13:54:28 +0530

    Medium .java Array

---
+++ LeetCode/Medium/0454-4sum-ii/0454-4sum-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer,Integer> map1 = new HashMap<>();
+
+        for(int i=0;i< nums1.length;i++){
+            for(int j=0;j<nums2.length;j++){
+                int sum =nums1[i]+nums2[j];
+                map1.put(sum,map1.getOrDefault(sum,0)+1);
+            }
+        }
+        int cnt=0;
+        for(int i=0;i<nums3.length;i++){
+            for(int j=0;j<nums4.length;j++){
+                int sum =nums3[i]+nums4[j];
+                cnt+=map1.getOrDefault(-sum,0);
+            }
+        }
+        return cnt;
+    }
+}
