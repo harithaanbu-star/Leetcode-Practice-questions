1ede79e83f706e77a836d3bd983caae4a0d4812e
Haritha <harithaanbu34@gmail.com>
2026-09-13 22:40:31 +0530

    Easy .java Array

---
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i=1;
+        int j=1;
+        while(j<nums.length){
+            if(nums[i-1]==nums[j]){
+                j++;
+            }else{
+                nums[i]=nums[j];
+                i++;
+                j++;
+            }
+        }
+        return i;
+    }
+}
