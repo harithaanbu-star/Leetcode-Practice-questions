400122bcab791d4ba404f789bc1af0b6fdd2d33e
Haritha <harithaanbu34@gmail.com>
2026-09-14 10:10:39 +0530

    Easy .java Math

---
+++ LeetCode/Easy/0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int l=Math.max(rec1[0],rec2[0]);
+        int b=Math.max(rec1[1],rec2[1]);
+        int r=Math.min(rec1[2],rec2[2]);
+        int t=Math.min(rec1[3],rec2[3]);
+
+        return l<r && b<t;
+    }
+}
