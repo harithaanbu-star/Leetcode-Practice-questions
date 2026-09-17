2918bb5e6d17b553ca83e9710eee39f33c7bc88d
Haritha <harithaanbu34@gmail.com>
2026-09-17 19:00:28 +0530

    Medium .java Hash Table

--- LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ LeetCode/Medium/0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,18 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int [] c= new int[26];
-        int i=0;
-        int cnt=0;
-        int maxcnt=0;
-        for(int j=0;j<s.length();j++){
-            c[s.charAt(j)-'A']++;
-            cnt=Math.max(cnt,c[s.charAt(j)-'A']);
-            while((j-i+1)-cnt>k){
-                c[s.charAt(i)-'A']--;
-                i++;
+        int st =0;
+        int end=s.length();
+        int maxwin=0;
+        int maxfreq=0;
+        int[] freq=new int[26];
+        for(int i=0;i<end;i++){
+            freq[s.charAt(i)-'A']++;
+            maxfreq=Math.max(maxfreq, freq[s.charAt(i)-'A']);
+            int wid =i-st+1;
+            if(wid-maxfreq>k){
+                freq[s.charAt(st)-'A']--;
+                st++;
             }
-            maxcnt=Math.max(maxcnt,(j-i+1));
-        }   
-        return maxcnt;
+            maxwin=Math.max(maxwin,i-st+1);
+        }
+        return maxwin;
     }
 }
