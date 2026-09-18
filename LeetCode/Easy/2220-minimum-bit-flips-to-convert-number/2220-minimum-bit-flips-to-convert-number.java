7455f8d89b7ad05471aa73df627937f4064173c6
Haritha <harithaanbu34@gmail.com>
2026-09-18 10:11:38 +0530

    Easy .java Bit Manipulation

--- LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ LeetCode/Easy/2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -1,5 +1,11 @@
 class Solution {
     public int minBitFlips(int start, int goal) {
-        return Integer.bitCount(start^goal);   
+        int x = start^goal;
+        int cnt =0;
+        while(x!=0){
+            cnt+=x & 1;
+            x>>=1;
+        }   
+        return cnt;
     }
 }
