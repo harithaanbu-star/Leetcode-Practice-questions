9315b37982adda737316fa187619f2a11fb7daa1
Haritha <harithaanbu34@gmail.com>
2026-09-25 22:30:40 +0530

    Easy .java Linked List

---
+++ LeetCode/Easy/0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dum = new ListNode(-101);
+        ListNode node=dum;
+        //ListNode last=null;
+        while(list1!=null && list2!=null){
+            if(list1.val<=list2.val){
+                node.next=list1;
+                node=list1;
+                list1=list1.next;
+            }else{
+                node.next=list2;
+                node=list2;
+                list2=list2.next;
+            }
+        }
+        while(list1!=null && list2==null){
+                node.next=list1;
+                node=node.next;
+                list1=list1.next;
+        }
+        while(list2!=null && list1==null){
+                node.next=list2;
+                node=node.next;
+                list2=list2.next;
+        }
+
+        return dum.next;
+    }
+}
