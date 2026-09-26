835953b58cfc3372245355ec62c6f12aa46739a9
Haritha <harithaanbu34@gmail.com>
2026-09-26 21:38:25 +0530

    Easy .java Linked List

---
+++ LeetCode/Easy/0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp=head;
+        ListNode first=temp;
+        ListNode second =temp;
+        while(second!=null && first!=null && second.next!=null){
+            first=first.next;
+            second=second.next.next;
+        }
+        return first;
+    }
+}
